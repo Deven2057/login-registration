336f171a30bb0b9103fb3e4558f756c49785e2df
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-11-09 23:54:50 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const signInButton= document.getElementById('signIn');
+const container= document.getElementById('container');
+signUpButton.addEventListener('click', () => {
+ container.classList.add("right-panel-active");
+});
