13719474d158b1647d98feb4302690ed71c077c2
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-11-09 23:55:12 +0545

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const container= document.getElementById('container');
 signUpButton.addEventListener('click', () => {
  container.classList.add("right-panel-active");
 });
+
+ container.classList.remove("right-panel-active");
+});
